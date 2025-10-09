1d5d82c79b19a81ec6cae89840aa69605db3dde8
User Aitalina <aitautau@gmail.com>
2025-10-09 14:22:04 +0500

    Проект №6 Проверка №1

--- src/ru/practicum/manager/TaskManager.java
+++ src/ru/practicum/manager/TaskManager.java
@@ -9,29 +9,45 @@ import java.util.List;
 public interface TaskManager {
     // методы ADD
     int addTask(Task task);
+
     int addEpic(Epic epic);
+
     int addSubTask(SubTask subtask);
 
     // методы UPDATE
     boolean updateTask(Task updatedTask);
+
     boolean updateEpic(Epic updatedEpic);
+
     boolean updateSubtask(SubTask updatedSubtask);
 
     // методы DELETE
     void deleteTask(int id);
+
     void deleteEpic(int id);
+
     void deleteSubtask(int id);
+
     void deleteAllSubtasks();
+
     void deleteAllEpics();
+
     void deleteAllTasks();
 
     // методы GET
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Epic getEpicById(int id);
+
     SubTask getSubtask(int id);
+
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<SubTask> getAllSubTasks();
+
     List<Task> getHistory();
 }
